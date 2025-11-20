9ea9d33bf78295e1733635c7f6dea215a124dcb2
EnugurthiAnup <eanup.736@gmail.com>
2025-11-20 23:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let table = document.querySelector('#sampleTable')
 
 function insert_Row() {
     //Write your code here
-	  table.innerHTML = 
-		    `<tr>
-            <td>New Cell1</td>
-            <td>New Cell2</td>
-        </tr>` + table.innerHTML;;
-		
+	  const newRow = table.insertRow(0);
+
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
 	
   
   
